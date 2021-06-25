5c17afdb384753bc2a1444c3e9fb283c63082575
Christopher Käding <19961227+Cuube@users.noreply.github.com>
2021-06-25 19:05:52 +0200

    Use Purpur instead of Paper

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
     maven("https://papermc.io/repo/repository/maven-public/")
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
     maven("https://m2.dv8tion.net/releases")
+    maven("https://repo.pl3x.net/")
     jcenter()
 }
 
@@ -18,7 +19,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    compileOnly("io.papermc.paper", "paper-api", "1.17-R0.1-SNAPSHOT")
+    compileOnly("net.pl3x.purpur", "purpur-api", "1.17-R0.1-SNAPSHOT")
     implementation("net.kyori:adventure-text-minimessage:4.1.0-SNAPSHOT")
     implementation("net.dv8tion:JDA:4.2.1_276")
     implementation("com.google.guava:guava:30.1.1-jre")
