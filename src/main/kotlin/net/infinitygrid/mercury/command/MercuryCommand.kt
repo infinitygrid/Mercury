bfae88bf30bab5936d6ec18ad850d4c943547f3e
Christopher Käding <19961227+Cuube@users.noreply.github.com>
2021-07-11 18:39:36 +0200

    Replace nullable MutableList with non-nullable MutableSet

--- src/main/kotlin/net/infinitygrid/mercury/command/MercuryCommand.kt
+++ src/main/kotlin/net/infinitygrid/mercury/command/MercuryCommand.kt
@@ -6,29 +6,25 @@ import org.bukkit.command.defaults.BukkitCommand
 
 abstract class MercuryCommand(name: String) : BukkitCommand(name) {
 
-    var requireAnyPermission: MutableList<String>? = null
-    val requirePermissions: MutableList<String>? = null
+    var requiresAnyPermission: MutableSet<String> = mutableSetOf()
+    val requiredPermissions: MutableSet<String> = mutableSetOf()
 
     final override fun execute(sender: CommandSender, commandLabel: String, args: Array<out String>): Boolean {
-        requireAnyPermission?.let {
-            if (!hasAnyPermission(sender, it)) return true
-        }
-        requirePermissions?.let {
-            if (!hasEveryPermission(sender, it)) return true
-        }
+        if (requiresAnyPermission.size > 0) if (!hasAnyPermission(sender, requiresAnyPermission)) return true
+        if (requiredPermissions.size > 0) if (!hasEveryPermission(sender, requiredPermissions)) return true
         return executeCommand(sender, commandLabel, args)
     }
 
     abstract fun executeCommand(sender: CommandSender, commandLabel: String, args: Array<out String>): Boolean
 
-    private fun hasEveryPermission(commandSender: CommandSender, nodeList: List<String>): Boolean {
+    private fun hasEveryPermission(commandSender: CommandSender, nodeList: Set<String>): Boolean {
         nodeList.forEach { permissionNode ->
             if (!commandSender.hasPermission(permissionNode)) return false
         }
         return true
     }
 
-    private fun hasAnyPermission(commandSender: CommandSender, nodeList: List<String>): Boolean {
+    private fun hasAnyPermission(commandSender: CommandSender, nodeList: Set<String>): Boolean {
         nodeList.forEach { permissionNode ->
             if (commandSender.hasPermission(permissionNode)) return true
         }
