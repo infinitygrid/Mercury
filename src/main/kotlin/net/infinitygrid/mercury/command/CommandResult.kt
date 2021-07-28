0619c47a37054e6672ab73c125d6b6b2fc5002c2
Christopher Käding <19961227+Cuube@users.noreply.github.com>
2021-07-28 18:59:20 +0200

    Add more CommandResult values

--- src/main/kotlin/net/infinitygrid/mercury/command/CommandResult.kt
+++ src/main/kotlin/net/infinitygrid/mercury/command/CommandResult.kt
@@ -2,6 +2,10 @@ package net.infinitygrid.mercury.command
 
 enum class CommandResult {
 
-    SUCCESS
+    SUCCESS,
+    NO_PERMISSION,
+    INSUFFICIENT_PERMISSIONS,
+    NOT_ENOUGH_ARGUMENTS,
+    SYNTAX_EXCEPTION
 
 }
